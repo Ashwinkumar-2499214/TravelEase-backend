6a0be85289ff2e46cc7e9ddab25552b18c2f0d41
Harshavardhan <2499131@cognizant.com>
2026-05-25 10:07:43 +0530

    Added files

--- Program.cs
+++ Program.cs
@@ -8,6 +8,8 @@ builder.Services.AddOpenApi();
 
 var app = builder.Build();
 
+System.Console.WriteLine("Hello World!");
+
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
